e17f5e71bd84f85866c24707da665addda7d6d23
anton8775 <yacenko372o@gmail.com>
2022-12-02 01:01:35 +0200

    Initial commit

---
+++ CoinInfoApp/CoinInfoApp/CoinInfoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinInfoAppApp.swift
+//  CoinInfoApp
+//
+//  Created by Антон Яценко on 02.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct CoinInfoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinInfoApp/CoinInfoApp/TitleAndSubtitle.swift
@@ -0,0 +1,20 @@
+//
+//  TitleAndSubtitle.swift
+//  CoinInfoApp
+//
+//  Created by Антон Яценко on 02.12.2022.
+//
+
+import SwiftUI
+
+struct TitleAndSubtitle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TitleAndSubtitle_Previews: PreviewProvider {
+    static var previews: some View {
+        TitleAndSubtitle()
+    }
+}
